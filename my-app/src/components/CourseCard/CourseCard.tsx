ccb28f0bec4b33c63b0b054ffbc07260c1cc4d34
Vladymyr <vladymyr.svyrydenko2004@gmail.com>
2023-06-13 14:22:52 +0300

    check-a

--- my-app/src/components/CourseCard/CourseCard.tsx
+++ my-app/src/components/CourseCard/CourseCard.tsx
@@ -1,10 +1,19 @@
+import React, { useEffect, useState } from 'react'
 import CourseDescription from '../CourseDescription/CourseDescription'
 import CourseImage from '../CourseImage/CourseImage'
-
 import s from './CourseCard.module.css'
 
 const CourseCard = ({ toLeft = false }: { toLeft?: boolean }) => {
-  const width = window.innerWidth
+  const [width, setWidth] = useState(0)
+
+  useEffect(() => {
+    setWidth(window.innerWidth)
+    const handleResize = () => setWidth(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return (
     <div className={`${s.courseCard} ${toLeft ? s.reverse : ''}`}>
