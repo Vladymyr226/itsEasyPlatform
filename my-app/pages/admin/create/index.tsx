1b38bfe96201d89165c9df22cfa54633c0ece529
Proveno <reznikmaksym14@gmail.com>
2024-06-27 22:52:49 +0200

    test media upload

--- my-app/pages/admin/create/index.tsx
+++ my-app/pages/admin/create/index.tsx
@@ -319,6 +319,13 @@ const CourseCreate = () => {
   const router = useRouter()
   const handleSubmit = async (e: any) => {
     // await uploadFileToS3(videoFile)
+    if (mediaValue?.content) {
+      console.log(mediaValue?.content)
+      const result = await uploadFileToS3(mediaValue?.content)
+      console.log(result)
+    }
+
+    return
     if (id && !editTrigger) {
       router.push('/admin')
       return
@@ -688,46 +695,47 @@ const CourseCreate = () => {
     }
   }
 
-  // if (
-  //   !process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID ||
-  //   !process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY ||
-  //   !process.env.NEXT_PUBLIC_AWS_BUCKET_NAME
-  // ) {
-  //   throw new Error(
-  //     'Отсутствуют переменные окружения NEXT_PUBLIC_AWS_ACCESS_KEY_ID или NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY или NEXT_PUBLIC_AWS_BUCKET_NAME'
-  //   )
-  // }
+  if (
+    !process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID ||
+    !process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY ||
+    !process.env.NEXT_PUBLIC_AWS_BUCKET_NAME
+  ) {
+    throw new Error(
+      'Отсутствуют переменные окружения NEXT_PUBLIC_AWS_ACCESS_KEY_ID или NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY или NEXT_PUBLIC_AWS_BUCKET_NAME'
+    )
+  }
 
-  // AWS.config.update({
-  //   accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-  //   secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
-  //   region: 'eu-west-3',
-  // })
+  AWS.config.update({
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+    region: 'eu-west-3',
+  })
 
-  // const s3 = new AWS.S3()
-  // const uploadFileToS3 = async (file: File) => {
-  //   console.log(file)
-  //   const uploadParams = {
-  //     Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME ?? '',
-  //     Key: `${file.name}`,
-  //     Body: file,
-  //     ContentType: file.type,
-  //     ACL: 'public-read',
-  //   }
+  const s3 = new AWS.S3()
+  const uploadFileToS3 = async (file: File) => {
+    console.log(file)
+    const uploadParams = {
+      Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME ?? '',
+      Key: `${file.name}`,
+      Body: file,
+      ContentType: file.type,
+      ACL: 'public-read',
+    }
+    console.log(uploadParams)
 
-  //   return new Promise((resolve, reject) => {
-  //     s3.upload(uploadParams, (err: any, data: any) => {
-  //       if (err) {
-  //         console.error('Ошибка загрузки файла:', err)
+    return new Promise((resolve, reject) => {
+      s3.upload(uploadParams, (err: any, data: any) => {
+        if (err) {
+          console.error('Ошибка загрузки файла:', err)
 
-  //         reject(`Ошибка загрузки файла: ${err}`)
-  //       } else {
-  //         console.log('Файл успешно загружен:', data.Location)
-  //         resolve(data.Location)
-  //       }
-  //     })
-  //   })
-  // }
+          reject(`Ошибка загрузки файла: ${err}`)
+        } else {
+          console.log('Файл успешно загружен:', data.Location)
+          resolve(data.Location)
+        }
+      })
+    })
+  }
 
   const [createLessonIndx, setCreateLessonIndx] = useState(-1)
   function compareIsLessonEdited(lessonId: string, moduleI: number) {
@@ -1043,7 +1051,7 @@ const CourseCreate = () => {
                 <Button fullWidth variant='contained' component='label' sx={{ mt: 1 }}>
                   <input
                     type='file'
-                    accept='video/mp4 image/png, image/jpeg'
+                    accept='video/mp4, image/png, image/jpeg'
                     onChange={handlePreviewMediaChange}
                   />
                 </Button>
